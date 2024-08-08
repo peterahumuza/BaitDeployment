c1c61a1546f3c9e0897bad2e69d8ab1931934929
Peter Ahumuza <peter@172-29-104-133.w.africa.local.cmu.edu>
2024-08-08 14:41:34 -0400

    missed deployment

--- components/students/Students.js
+++ components/students/Students.js
@@ -17,6 +17,7 @@ import {
     TableContainer,
 } from '@chakra-ui/react'
 
+
 import ModifyStudent from './ModifyStudent';
 import StudentProfile from './StudentProfile';
 import WarningModal from '@/components/systemmessages/WarningModal';
